9511ef7e52e9adbf8ca5d857eb1c9456462f8965
Andrés Arias <arias9306@gmail.com>
2024-11-28 16:16:37 +0100

    feat(ngrx-signals): create with filter feature

--- apps/ngrx-signals/src/app/movies/store/movie.store.ts
+++ apps/ngrx-signals/src/app/movies/store/movie.store.ts
@@ -1,4 +1,5 @@
-import { patchState, signalStore, withMethods } from '@ngrx/signals';
+import { computed } from '@angular/core';
+import { patchState, signalStore, signalStoreFeature, type, withComputed, withMethods, withState } from '@ngrx/signals';
 import { addEntity, withEntities } from '@ngrx/signals/entities';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { pipe, tap } from 'rxjs';
@@ -13,3 +14,26 @@ export const MovieStore = signalStore(
     add: rxMethod<Movie>(pipe(tap((movie: Movie) => patchState(store, addEntity(movie))))),
   }))
 );
+
+export type FilterState = { searchTerm: string };
+
+export interface Entity {
+  name: string;
+}
+
+export function withFilter() {
+  return signalStoreFeature(
+    {
+      state: type<{ items: Entity[] }>(),
+    },
+    withState<FilterState>({ searchTerm: '' }),
+    withComputed(({ items, searchTerm }) => ({
+      filteredItems: computed(() => {
+        const term = searchTerm().toLowerCase();
+        return items().filter((item: Entity) => item.name.toLowerCase().includes(term));
+      }),
+    }))
+  );
+}
+
+export const UsersStore = signalStore(withState({ items: [] }), withFilter());
